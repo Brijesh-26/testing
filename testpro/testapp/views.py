f70f7d375c83663af878adee8cff38b957be5df8
Brijesh-26 <brijeshkumarv598@gmail.com>
2024-04-22 13:27:35 +0530

    first testing commit

--- testpro/testapp/views.py
+++ testpro/testapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from rest_framework import status, generics
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 # Create your views here.
+
+class TestAPIView(APIView):
+
+    def get(self, request):
+        response_data= {
+            "message": "this is testing"
+        }
+        return Response(response_data, status=200)
